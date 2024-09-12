2e608853ad106088a9fdcd165edbd608df7c9b20
Administrator <root>
2024-09-12 08:48:29 +0800

    imported authMiddle in group.js

--- backend/routes/group.js
+++ backend/routes/group.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const groupController = require('../controllers/groupControllers')
+const groupController = require('../controllers/groupControllers');
+const authMiddle = require('../middlewares/authMiddle');
 
 router.post("/createNewGroup", authMiddle.verifyToken, groupController.createNewGroup);
 router.get("/getAllUserGroup", authMiddle.verifyToken, groupController.getAllUserGroup);
